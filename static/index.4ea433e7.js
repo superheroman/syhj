b04e342450965c73204e5c8a1f7aa80b6aaad5da
NBSeven <410805964@qq.com>
2022-06-01 13:40:14 +0000

    Deploying to gh-pages from @ NBSeven/syhj@91a7fc32b11b8c79fddc5d064c28ae0b86852247 🚀

---
+++ static/index.4ea433e7.js
@@ -0,0 +1,1 @@
+import{e as a,r as e,o as r,c as s,g as n,w as t}from"./index.8fc807ae.js";const o={class:"app-container"};var c=a({},[["render",function(a,c){const d=e("router-view"),i=e("el-card");return r(),s("div",o,[n(i,{header:"menu 1"},{default:t((()=>[n(d)])),_:1})])}]]);export{c as default};
