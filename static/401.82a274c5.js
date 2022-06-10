cb1d053747c80a51aed20943a14e48cbda25662c
NBSeven <410805964@qq.com>
2022-06-10 07:42:16 +0000

    Deploying to gh-pages from @ NBSeven/syhj@ebc73ca9ef1546afd9f66704d0c9ee741ca3ac96 🚀

---
+++ static/401.82a274c5.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,b as n,i as r}from"./index.57161f6f.js";const s={},a=[r("p",{style:{"text-align":"center","font-size":"140px","margin-bottom":"50px"}},"401",-1),r("p",{style:{"text-align":"center","font-size":"40px"}},"你没有权限去该页面",-1)];var i=t(s,[["render",function(t,r){return e(),n("div",null,a)}]]);export{i as default};
