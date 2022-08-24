ceee1fec47e6548471db747316ca6611a6248b61
ljc <747301367@qq.ccom>
2022-08-24 23:50:30 +0800

    fix: 联调版本管理

--- src/views/versionManagement/common/const.ts
+++ src/views/versionManagement/common/const.ts
@@ -1,44 +1,12 @@
 /**版本管理 */
 // 版本管理 - 筛选项初始数据
 export const InitVersionFilterValue = {
-  projectName: "",
-  versionNo: "",
-  processNo: "",
-  draftTime: [],
-  pricingEndTime: [],
-  quotationEndTime: []
+  ProjectName: "",
+  Version: "",
+  AuditFlowId: "",
+  DraftDate: [],
+  FinishedDate: []
 }
-
-// 版本管理 - 筛选项
-export const VerisonfilterNnum = [
-  {
-    label: "项目名称",
-    key: "projectName"
-  },
-  {
-    label: "版本号",
-    key: "versionNo"
-  },
-  {
-    label: "流程单号",
-    key: "processNo"
-  },
-  {
-    label: "拟稿时间",
-    key: "draftTime",
-    role: "timePicker"
-  },
-  {
-    label: "核价节点结束时间",
-    key: "pricingEndTime",
-    role: "timePicker"
-  },
-  {
-    label: "报价结束时间",
-    key: "quotationEndTime",
-    role: "timePicker"
-  }
-]
 
 /**报表查询 */
 
