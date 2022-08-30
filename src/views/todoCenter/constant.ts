43f936aeae2c0df1b0eb9257f52ca599ca2f7272
wangzhongxiang <wangzhongxiang@duiba.com.cn>
2022-08-30 10:27:44 +0800

    fix: 修改nre跳转页面

--- src/views/todoCenter/constant.ts
+++ src/views/todoCenter/constant.ts
@@ -12,13 +12,13 @@ const urlMap = {
   ManHourImport: "/processImport/index", // 工序导入
   LogisticsCostInput: "/pmDepartment/index", //物流成本录入界面
   NreInputOther: "/nre/nreProjectManageMent", //NRE核价手板件、差旅及其他费用录入界面
-  NreInputEmc: "", //NRE核价电子-EMC实验费录入界面"
+  NreInputEmc: "/nre/nrePilotprojects", //NRE核价电子-EMC实验费录入界面"
   NreInputMould: "/nre/nreResourcesDepartment", //NRE模具清单录入界面
-  NreInputTest: "/nre/nrePilotprojects", //NRE实验费用录入界面
+  NreInputTest: "/nre/nreExperimentItems", //NRE实验费用录入界面
   NreInputGage: "/nre/nreQCDepartment", //NRE检具费用录入界面"
   TradeApproval: "/tradeCompliance/index", //贸易合规审核（结束流程&退回流程）
   PriceBoardAudit: "/dashboard", //核价看板
-  PriceCheckDownload: "", //"版本的核价表查看&下载界面（产品+NRE）"
+  PriceCheckDownload: "/quoteAnalysis/index", //"版本的核价表查看&下载界面（产品+NRE）"
   CostCheckNreFactor: "", //"成本信息表下载&填报NRE报价系数&产品报价看板界面"
   QuoteApproval: "", //"总经理报价审批界面"
   QuoteFormCheckDownload: "", //"报价审核表查看&下载界面"
