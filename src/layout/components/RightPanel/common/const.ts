bdca29d412e009b5a5c788c273f9411cbfc65b88
ljc <747301367@qq.ccom>
2022-09-09 08:48:45 +0800

    fix: bugfix

--- src/layout/components/RightPanel/common/const.ts
+++ src/layout/components/RightPanel/common/const.ts
@@ -1,9 +1,11 @@
 export const wahiteRotes: any[] = [
   "demandApply", // 营销部录入
   "managerOperate", // 经理部录入
-  "trAudit", // tr 主方案
+  // "trAudit", // tr 主方案
   "ProductPriceList", // 项目核价表
   // "quoteAnalysis", // 报价分析看板
+  "electronicImport", // 电子料导入
+  "structuralMaterialImport", // 结构料导入
   "electronic", // bom单价录入 - 电子料录入
   "construction", // bom单价录入 - 结构bom录入
   "bomVerifyElectronic", // bom单价审核 - 电子料单价审核
