53afe662b0fd4f75b1c958da4ccd431e669c5829
wangzhongxiang <wangzhongxiang@duiba.com.cn>
2022-09-27 17:47:26 +0800

    fix: 下载统一加上loading

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -3,7 +3,7 @@ import { useUserStoreHook } from "@/store/modules/user"
 import { ElMessage } from "element-plus"
 import { get } from "lodash-es"
 import { getToken } from "@/utils/cookies"
-// import { ElLoading } from "element-plus"
+import { ElLoading } from "element-plus"
 
 /** 创建请求实例 */
 function createService() {
@@ -108,6 +108,7 @@ function createService() {
 
 /** 创建请求方法 */
 function createRequestFunction(service: AxiosInstance) {
+  let loadingInstance: any = null
   return function (config: AxiosRequestConfig) {
     const configDefault = {
       headers: {
@@ -116,19 +117,24 @@ function createRequestFunction(service: AxiosInstance) {
         "Content-Type": get(config, "headers.Content-Type", "application/json")
       },
       timeout: 300000,
-      baseURL: import.meta.env.VITE_BASE_API,
-      data: {}
-      // onDownloadProgress: function (progressEvent: any) {
-      //   // 对原生进度事件的处理
-      //   const loadingInstance = ElLoading.service({
-      //     fullscreen: true,
-      //     text: "加载中"
-      //   })
-      //   console.log(((progressEvent.loaded / progressEvent.total) * 100).toFixed(2) + "%")
-      //   if (((progressEvent.loaded / progressEvent.total) * 100).toFixed(2) === "100.00") {
-      //     loadingInstance.close()
-      //   }
-      // }
+      // baseURL: import.meta.env.VITE_BASE_API,
+      data: {},
+      onDownloadProgress: function (progressEvent: any) {
+        // 对原生进度事件的处理
+        if (config.responseType === "blob") {
+          if (!loadingInstance) {
+            loadingInstance = ElLoading.service({
+              fullscreen: true,
+              text: "下载中"
+            })
+          }
+          console.log(((progressEvent.loaded / progressEvent.total) * 100).toFixed(2) + "%")
+          if (((progressEvent.loaded / progressEvent.total) * 100).toFixed(2) === "100.00") {
+            loadingInstance.close()
+            loadingInstance = null
+          }
+        }
+      }
       // onUploadProgress: function (progressEvent: any) {
       //   const loadingInstance = ElLoading.service({
       //     fullscreen: true,
