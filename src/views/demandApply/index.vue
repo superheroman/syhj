62173fe43c01ce3623e69f1607f5f180159e3c94
wangzhongxiang <wangzhongxiang@duiba.com.cn>
2022-10-11 16:25:45 +0800

    feat: 优化显示

--- src/views/demandApply/index.vue
+++ src/views/demandApply/index.vue
@@ -836,7 +836,7 @@ const formatThousandths = (_record: any, _row: any, cellValue: any) => {
   if (cellValue) {
     return (Number(cellValue).toFixed(2) + "").replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, "$&,")
   } else {
-    return 0
+    return ""
   }
 }
 const modelCountYearListQuantitySum = (row: modelCount) => {
